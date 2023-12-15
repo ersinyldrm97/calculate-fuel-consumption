27d85e1d2378df4a973d70351958e4941b4a5e8f
Ersin <respawm19@gmail.com>
2023-12-15 20:05:15 +0300

    changed Aydın to Aydin

--- calculate_fuel_consumption.py
+++ calculate_fuel_consumption.py
@@ -9,19 +9,19 @@ def calculate_and_print(total_km, liters_per_100_km, petrol_price):
 
 def main():
     try:
-        aydin_to_istanbul_total_km = 550
+        aydin_to_istanbul_total_km = 547
         istanbul_to_igneada_total_km = 262
         aydin_to_istanbul_igneada_total_km = 809
         liters_per_100_km = 7.5
         petrol_price = 33.89
         
-        print("Traveling from Aydın to Istanbul:")
+        print("Traveling from Aydin to Istanbul:")
         calculate_and_print(aydin_to_istanbul_total_km, liters_per_100_km, petrol_price)
 
         print("\nTraveling from Istanbul to Igneada:")
         calculate_and_print(istanbul_to_igneada_total_km, liters_per_100_km, petrol_price)
 
-        print("\nTraveling from Aydın to Igneada:")
+        print("\nTraveling from Aydin to Igneada:")
         calculate_and_print(aydin_to_istanbul_igneada_total_km, liters_per_100_km, petrol_price)
 
     except ValueError:
